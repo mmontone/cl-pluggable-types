a775f1eff6d13b17b64d02b4b5e088064ecdfa8c
mmontone <marianomontone@gmail.com>
2013-08-29 14:09:10 -0300

    Compilation fix

--- gradual-common-lisp.lisp
+++ gradual-common-lisp.lisp
@@ -6,6 +6,6 @@
 (defmacro defparameter (name &optional value type)
   `(gradual::typed-defparameter ,name ,value ,type))
 
-(do-external-symbols (s :common-lisp)
-  (export (find-symbol (symbol-name s)) :gradual-common-lisp))
-
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (do-external-symbols (s :common-lisp)
+    (export (find-symbol (symbol-name s)) :gradual-common-lisp)))
