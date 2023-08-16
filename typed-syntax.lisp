49a79ac4f1fbd90c8592e540a238afb7849e247c
Mariano Montone <marianomontone@gmail.com>
2023-08-16 09:05:13 -0300

    Typed syntax

--- typed-syntax.lisp
+++ typed-syntax.lisp
@@ -22,10 +22,9 @@
           tree)
       (let ((car (tree-remove-if predicate (car tree)))
             (cdr (tree-remove-if predicate (cdr tree))))
-        (cond
-          ((and car cdr)
-           (cons car cdr))
-          (car car)))))
+        (if (null car)
+            cdr
+            (cons car cdr)))))
 
 (tree-remove-if (lambda (x) (eql 'foo x)) '(foo))
 (tree-remove-if (lambda (x) (eql 'foo x)) '(foo foo))
@@ -33,23 +32,48 @@
 (tree-remove-if (lambda (x) (eql 'foo x)) '(x (foo foo)))
 (tree-remove-if (lambda (x) (eql 'foo x)) '(x (y foo foo)))
 
-(cl:defun type-annotation-p (thing)
-  (and ))
+(tree-remove-if #'vectorp '(x #(integer) y #(string)))
 
+(defstruct type-annotation
+  type)
 
 (cl:defun remove-type-annotations (def)
-  def)
+  (let ((annotated-def (parse-type-annotations def)))
+    (destructuring-bind (def name annotated-args &body annotated-body)
+        annotated-def
+      `(,def ,name ,(tree-remove-if #'type-annotation-p annotated-args)
+         ,@(if (type-annotation-p (first annotated-body))
+              (rest annotated-body)
+              annotated-body)))))
+
+(remove-type-annotations
+ '(defun hello (x <integer>)
+   (print "hello")))
+
+(remove-type-annotations
+ '(defun hello (x <integer> y <string>)
+   (print "hello")))
+
+(remove-type-annotations
+ '(defun hello (x <list-of string>) <string>
+   (first x)))
+
+(remove-type-annotations
+ '(defun hello (x <list-of string> &optional (y <string> "lala")) <string>
+   (first x)))   
 
 (cl:defun parse-type-annotations (list-of-symbols)
   (let* ((str (prin1-to-string list-of-symbols))
-         (str (ppcre:regex-replace-all "\\<" str "#("))
-         (str (ppcre:regex-replace-all ">" str ")")))
+         (str (ppcre:regex-replace-all "\\<" str "#S(TYPE-ANNOTATION :TYPE ("))
+         (str (ppcre:regex-replace-all ">" str "))")))
     (read-from-string str)))
 
 (parse-type-annotations (read-from-string "(<list-of number>)"))
 (parse-type-annotations (read-from-string "(<number>)"))
 
 (parse-type-annotations (read-from-string "(<list-of <cons-of integer string>>)"))
+
+(parse-type-annotations (read-from-string "(<list-of (cons-of integer string)>)"))
 
 (defmacro defun (name args &body body)
   "Define a function allowing type annotations.
